cb03e7a5d37f4da40ad72f6afe0788b4cf32420a
Shar McLeod <shr.mcl@gmail.com>
2022-12-18 12:51:43 -0600

    gathered data from api

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-let endpoint = 'https://pokeapi.co/api/v2/pokemon/ditto'
+// stackoverflow random number code
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-fetch(endpoint).then((res) => res.json()).then((data) => console.log('the data = ', data))
+let randomNum = randomIntFromInterval(1,100)
+
+let endpoint = `https://pokeapi.co/api/v2/pokemon/${randomNum}`
+
+fetch(endpoint)
+    .then((res) => res.json())
+        .then((data) => 
+            console.log('the data = ', data, 
+            'the name = ', data.name,
+            'the species = ', data.species.name,
+            'img = ', data.sprites.other.dream_world.front_default
+            )
+        )
