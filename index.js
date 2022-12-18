ca361ef5b4f8eb80232b7825313a785ef39d4c92
Shar McLeod <shr.mcl@gmail.com>
2022-12-18 13:27:56 -0600

    styled pokemon on page

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // stackoverflow random number code
 function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
-let randomNum = randomIntFromInterval(1, 100);
-let endpoint = `https://pokeapi.co/api/v2/pokemon/${randomNum}`;
 
-
-fetch(endpoint)
+// wait for page to load before fetching data
+window.addEventListener('load', () => {
+    let randomNum = randomIntFromInterval(1, 100);
+    let endpoint = `https://pokeapi.co/api/v2/pokemon/${randomNum}`;      
+    
+    fetch(endpoint)
     .then((res) => res.json())
     .then((data) => {
-        console.log(data);
-
+        console.log('fetched data: ', data);
         const pokeName = data.name;
         const pokeImg = data.sprites.other.dream_world.front_default;
         const pokeHeight = data.height
@@ -23,18 +24,15 @@ fetch(endpoint)
         const newP_height = document.createElement('p');
 
         newImg.src = pokeImg;
+        newImg.className = "pokemonImg";
         newH1.innerHTML = pokeName;
-        newP_height.innerHTML = pokeHeight;
-        newP_weight.innerHTML = pokeWeight;
+        newP_height.innerHTML = `height: ${pokeHeight}`;
+        newP_weight.innerHTML = `weight: ${pokeWeight}`;
 
         display.append(newImg);
         display.append(newH1);
-        display.append(newP_weight);
         display.append(newP_height);
-
-        // console.log('the data = ', data, 
-        //     'the name = ', data.name,
-        //     'the species = ', data.species.name,
-        //     'img = ', data.sprites.other.dream_world.front_default
-        //     )
+        display.append(newP_weight);
     })
+})
+
